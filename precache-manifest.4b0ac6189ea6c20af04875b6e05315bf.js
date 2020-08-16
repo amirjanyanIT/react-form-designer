75007fbb4eea1c19dad720225cad159fd363c5c7
AmirjanyanIT <amirjanyanit@gmail.com>
2020-08-17 00:07:51 +0400

    Updates

---
+++ precache-manifest.4b0ac6189ea6c20af04875b6e05315bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a36f9324fb9f4970d7f1bfa34277fc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7c1390968e7096c324de",
+    "url": "./static/js/2.c9543ffa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.c9543ffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bab7199fefcf9a062a39",
+    "url": "./static/js/main.6d2d321c.chunk.js"
+  },
+  {
+    "revision": "71ad0e29a44f0188481d",
+    "url": "./static/js/runtime-main.54425770.js"
+  }
+]);
